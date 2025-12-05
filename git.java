45c04845386f87e650c5cc478dbb1da76625cbf7
Isabelle-cell <isabelle53814366@aluno.ba.senac.br>
2025-12-05 15:07:27 -0300

    Add files via upload

---
+++ git.java
@@ -0,0 +1,23 @@
+package github;
+
+import java.util.Scanner;
+public class git {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número inteiro: ");
+        int numero1 = scanner.nextInt();
+
+        System.out.print("Digite o segundo número inteiro: ");
+        int numero2 = scanner.nextInt();
+
+        int soma = numero1 + numero2;
+
+        System.out.println("A soma dos dois números é: " + soma);
+
+        scanner.close();
+	}
+
+}
